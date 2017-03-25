ce9d6628d4a4e07bdd973c32fdc77d4a773e2cd3
Julianne Tan <Julianne Tan>
2017-03-25 10:56:51 +0800

    Finally pushed v1 D:

---
+++ Client/src/MTPlayer/MTDriver.java
@@ -0,0 +1,13 @@
+package MTPlayer;
+
+import javax.swing.JFrame;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class MTDriver {
+	public static void main(String[] args)  throws IOException {
+		MTClient player = new MTClient();
+	}
+}
+
